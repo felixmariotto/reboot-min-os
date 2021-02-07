c9d219fab999c3a24addc6a78afe4908ae46ad20
Felix Mariotto <felixmariotto@gmail.com>
2021-02-07 14:17:40 +0100

    initial comit

---
+++ config/webpack.config.js
@@ -0,0 +1,79 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+//
+
+module.exports = env => {
+
+	let mode = "development";
+	let devtool = 'eval-source-map';
+	let minimize = false;
+
+	const environment = JSON.stringify( env.NODE_ENV );
+
+	// Prod environment
+	if ( env.NODE_ENV === 'prod' ) {
+		devtool = false;
+		// comment these two lines out to keep Webpack from minimizing the code :
+		minimize = true;
+		mode = 'production';
+	};
+
+	return {
+
+		mode: mode,
+
+		entry: {
+			'bundle': './src/index.js',
+			'engine': './src/engine/engine.js',
+		},
+
+		output: {
+			filename: '[name].js',
+		}, 
+
+		devServer: {
+			contentBase: './dist'
+		},
+
+		optimization: {
+			minimize: minimize
+		},
+
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: './src/index.html',
+				inject: false
+			}),
+			// 'prod' or 'dev', bundled in the modules as the __API__ global variable.
+			// We use this to know which domain to connect to with socket.io, among other things.
+			new webpack.DefinePlugin({
+				__API__: environment
+			})
+		],
+
+		devtool: devtool,
+
+		module: {
+
+			rules: [
+
+				{
+					test: /\.(png|jpg|)$/,
+					use: [
+						'file-loader',
+					],
+				},
+
+				{
+					test: /\.css$/i,
+					use: ['style-loader', 'css-loader'],
+				},
+
+			],
+
+		}
+
+	}
+
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log( 'site entry' );
+
+window.addEventListener( 'engine-loaded', () => {
+
+	console.log( 'can start game' );
+
+} );

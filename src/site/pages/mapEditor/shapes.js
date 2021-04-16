a0dc5f56f9f8e58bf1b65b2f45c4436cf7e6ed3f
Felix Mariotto <felixmariotto@gmail.com>
2021-04-16 14:23:09 +0200

    add creation tools buttons in editors's shapes menu

--- src/site/pages/mapEditor/shapes.js
+++ src/site/pages/mapEditor/shapes.js
@@ -1,5 +1,6 @@
 
 import { elem, icon } from '../../utils.js';
+import Button from '../../components/button/Button.js';
 import './shapes.css';
 
 //
@@ -8,7 +9,47 @@ const shapes = [];
 
 const shapesOptions = elem({ id: 'editor-shapes-options', classes: 'tool-options' });
 
-shapesOptions.append('shapes options')
+//
+
+const addShapeTools = elem({ id: 'editor-add-shape-toolbar' });
+
+addShapeTools.append(
+	makeShapeCreatorButton( 'fas fa-circle', createSphere ),
+	makeShapeCreatorButton( 'far fa-cube', createBox )
+);
+
+function makeShapeCreatorButton( iconClasses, callback ) {
+
+	const btnIcon = icon( iconClasses );
+
+	const button = Button( btnIcon );
+
+	button.onclick = callback;
+
+	return button
+
+}
+
+//
+
+shapesOptions.append(
+	'Add a shape :',
+	addShapeTools
+);
+
+//
+
+function createSphere() {
+
+	console.log('create a sphere');
+
+}
+
+function createBox() {
+
+	console.log('create a box');
+
+}
 
 //
 

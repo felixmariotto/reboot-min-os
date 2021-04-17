dbe81dbab8eb0251791a4f6925c225a7bd0d355b
Felix Mariotto <felixmariotto@gmail.com>
2021-04-17 17:07:44 +0200

    editor: add buttons to files panel

--- src/site/pages/mapEditor/files.js
+++ src/site/pages/mapEditor/files.js
@@ -1,10 +1,44 @@
 
 import './files.css';
+import Button from '../../components/button/Button.js';
 import { elem, icon } from '../../utils.js';
 
 //
 
 const filesOptions = elem({ id: 'editor-files-options', classes: 'tool-options' });
+
+filesOptions.append(
+	makeButton( 'save as JSON', saveAsJSON ),
+	makeButton( 'import JSON', importJSON )
+);
+
+function makeButton( text, callback ) {
+
+	const button = Button( text );
+
+	button.onclick = callback;
+
+	return button
+
+}
+
+//
+
+function saveAsJSON() {
+
+	console.log( 'save as JSON' );
+
+}
+
+function importJSON() {
+
+	if ( window.confirm( 'Are you sure to import a scene and erase the current scene ?' ) ) {
+
+		console.log( 'import JSON' );
+		
+	}
+
+}
 
 //
 

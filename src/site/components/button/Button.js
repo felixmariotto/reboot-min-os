66dac3dee1afb34d51fba0342817913f4d5877de
Felix Mariotto <felixmariotto@gmail.com>
2021-06-06 21:30:34 +0200

    move list buttons with the gamepad

--- src/site/components/button/Button.js
+++ src/site/components/button/Button.js
@@ -6,7 +6,9 @@ import { elem } from '../../utils.js';
 
 export default function Button( content ) {
 
-	const domElement = elem({ classes: 'button', html: content });
+	const domElement = elem({ tagName: 'BUTTON', classes: 'button', html: content });
+
+	domElement.isSelectable = true;
 
 	return domElement
 
--- src/site/components/list/List.js
+++ src/site/components/list/List.js
@@ -4,23 +4,72 @@
 export default function List() {
 
 	let listening, handling;
+	let currentElement = 0;
 
 	const elem = document.createElement('DIV');
 
-	// enable gamepad control
+	//
+
+	function select() {
+
+		const selectables = getSelectables();
+
+		selectables[ currentElement ].focus();
+
+	}
+
+	function getSelectables() {
+
+		const children = Array.from( elem.children );
+
+		return children.filter( e => e.isSelectable );
+
+	}
+
+	// Called on joystick hits up or down.
+
+	function moveUp() {
+
+		currentElement --;
+
+		if ( currentElement < 0 ) currentElement = 0;
+
+		select();
+
+	}
+
+	function moveDown() {
+
+		const selectables = getSelectables();
+
+		currentElement ++;
+
+		if ( currentElement > selectables.length - 1 ) {
+
+			currentElement = selectables.length - 1;
+
+		}
+
+		select();
+
+	}
+
+	// Gamepad controls enabling/disabling.
+	// Only enabled when the user is in the menu and actively browsing it.
+
 	elem.enable = function () {
 
 		if ( !listening ) {
 
 			engine.on( 'joystick-hit-up', () => {
 
-				if ( handling ) console.log('move up')
+				if ( handling ) moveUp();
 
 			} );
 
 			engine.on( 'joystick-hit-down', () => {
 
-				if ( handling ) console.log('move down')
+				if ( handling ) moveDown();
 
 			} );
 

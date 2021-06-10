77af45f62728c412d2a59ecd4abf335048be5e47
Felix Mariotto <felixmariotto@gmail.com>
2021-06-10 09:55:15 +0200

    homepage screen: fix background position

--- src/site/pages/homepage/homepage.js
+++ src/site/pages/homepage/homepage.js
@@ -40,8 +40,9 @@ function backgroundLoop() {
 	if ( !mustStopBackgroundLoop ) {
 		if ( targetTopPos ) {
 			const newTopPos = lastTopPos + ( ( targetTopPos - lastTopPos ) * 0.1 );
-			homepageBack.style.transform = `translateX( ${ newTopPos }% )`;
+			homepageBack.style.transform = `translateX( calc( -50% + ${ newTopPos }% ) )`;
 			lastTopPos = newTopPos;
+			// console.log( 'translation : ' + newTopPos + '%')
 		}
 		if ( targetBackPos ) {
 			const newBackPos = lastBackPos + ( ( targetBackPos - lastBackPos ) * 0.1 );
